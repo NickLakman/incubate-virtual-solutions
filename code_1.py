95345dbe6f3340e5d07f4337038a97c6e1c7d1cf
NickLakman <140800149+NickLakman@users.noreply.github.com>
2023-11-25 23:34:54 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,27 @@
+    data = generate_random_data()
+def main():
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    return data
+
+    for item in data:
+def generate_random_data():
+
+    main()
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+import random
+
+def generate_random_data():
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+def main():
+
+
+    data = generate_random_data()
+    main()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
