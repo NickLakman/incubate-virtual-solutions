a7ee1a904bace1473f6d4c5c8b0b3d27ca48dbb9
NickLakman <140800149+NickLakman@users.noreply.github.com>
2023-11-28 18:31:39 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,39 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+    for item in data:
+    data = generate_random_data()
+    main()
+if __name__ == "__main__":
+
+
+def main():
+import random
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        int[] data = new int[10];
+    }
+
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+}
+public class RandomDataGenerator {
+        }
+        }    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+import random
+    main()
+
+
+    for item in data:
+if __name__ == "__main__":
+
+    return data
